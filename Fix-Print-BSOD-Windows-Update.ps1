663b02f46f1fc0b612d207f8a46d29af0bb1cd95
Drew Markel <drewmrk8@gmail.com>
2021-03-16 09:35:34 -0400

    Fix issues
    Fixed issues of restarting before the updates were uninstalled (will now restart in 10 minutes from the time the script has been opened)  and uninstallation of updates are no longer quiet as to get better feedback on the uninstallation process.

--- Fix-Print-BSOD-Windows-Update.ps1
+++ Fix-Print-BSOD-Windows-Update.ps1
@@ -4,30 +4,31 @@ echo "Windows Version"
 echo $version
 echo "-----------------"
 
-If ($version -eq 1803 -or $version -eq 1809 -or $version -eq 1909 -or $version -eq 2004) {
-
-  If ($version -eq 1803) {
-    echo "Uninstalling KB5000809..."
-    wusa /uninstall /kb:50000809 /quiet /norestart
-  } Elseif ($version -eq 1809) {
-    echo "Uninstalling KB5000822..."
-    wusa /uninstall /kb:5000822 /quiet /norestart
-  } Elseif ($version -eq 1909) {
-    echo "Uninstalling KB5000808..."
-    wusa /uninstall /kb:5000808 /quiet /norestart
-  } Elseif ($version -eq 2004) {
-    echo "Uninstalling KB5000802..."
-    wusa /uninstall /kb:5000802 /quiet /norestart
-  }
+If ($version -eq 1803 -or $version -eq 1809 -or $version -eq 1909 -or $version -eq 2004 -or $version -eq 2009) {
 
   echo "Pausing Windows updates until 4/15/2021..."
-  Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' -Name 'PauseUpdatesExpiryTime' -Value "2021-04-16T00:00:00Z"
+	Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings' -Name 'PauseUpdatesExpiryTime' -Value "2021-04-16T00:00:00Z"
 
-  echo "All done, restarting now!"
-  Restart-Computer
+	If ($version -eq 1803) {
+		echo "Uninstalling KB5000809..."
+		wusa /uninstall /kb:50000809
+	} Elseif ($version -eq 1809) {
+		echo "Uninstalling KB5000822..."
+		wusa /uninstall /kb:5000822
+	} Elseif ($version -eq 1909) {
+		echo "Uninstalling KB5000808..."
+		wusa /uninstall /kb:5000808
+	} Elseif ($version -eq 2004 -or $version -eq 2009) {
+		echo "Uninstalling KB5000802..."
+		wusa /uninstall /kb:5000802
+	}
+
+  echo "Restarting in 10 minutes..."
+  Start-Sleep -Seconds 600; Restart-Computer -Force
+
 
 } else {
-  echo "You do not have an effected version of Windows."
+	  echo "You do not have an effected version of Windows."
 }
 
 pause
